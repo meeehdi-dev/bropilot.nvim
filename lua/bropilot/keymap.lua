baf0ca431b427f1adf2ea6a3ed1d842c58d2be88
meeehdi <mehdi.mhiri@gmail.com>
2024-07-04 10:15:56 +0200

    doc(keymap): add comments

--- lua/bropilot/keymap.lua
+++ lua/bropilot/keymap.lua
@@ -8,9 +8,12 @@ M.set = function(keycode, cb)
   end
 
   vim.keymap.set("i", keycode, function()
+    -- if the callback already handled the keymap
     if cb() then
       return
     end
+
+    -- else we fallback on vim default action
     local keys = vim.api.nvim_replace_termcodes(keycode, true, false, true)
     vim.api.nvim_feedkeys(keys, "n", true)
   end, { noremap = true })
